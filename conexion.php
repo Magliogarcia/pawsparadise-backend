e461786e3487518b44bd05a21b0319fc46ff85c2
Magliogarcia <147286476+Magliogarcia@users.noreply.github.com>
2026-05-02 06:12:37 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -9,8 +9,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
 
 // Datos de Railway (los reemplazas con tus valores del PASO 2)
-$host     = "mysql://root:odaWlwLmgJPVrmuaCidtoZaNBHvbljVT@switchyard.proxy.rlwy.net:24146/railway";
-$port     = "3306";
+$host     = "switchyard.proxy.rlwy.net";
+$port     = "24146";
 $dbname   = "railway";
 $username = "root";
 $password = "odaWlwLmgJPVrmuaCidtoZaNBHvbljVT";
